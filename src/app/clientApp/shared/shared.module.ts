50bc2eb22e80ab03660821ea1d5babd5c9472dfe
wicho <wichocanul07@gmail.com>
2023-10-05 17:51:13 -0600

    SAF: improve nav component and install popper js

--- src/app/clientApp/shared/shared.module.ts
+++ src/app/clientApp/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
+import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -9,7 +10,8 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ],
   exports: [
     NavComponent
