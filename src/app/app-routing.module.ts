c1060e6dcd0aa13c633c65e13c557176d9792cbe
wicho <wichocanul07@gmail.com>
2023-10-04 21:00:36 -0600

    SAF: Improve styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {
