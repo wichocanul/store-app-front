e79d9e2ef860374ed5cc29a8cf06ce0d52c0324c
wicho <wichocanul07@gmail.com>
2023-10-06 00:30:32 -0600

    SAF: Create category component and module

---
+++ src/app/clientApp/pages/category/category-main/category-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-main',
+  templateUrl: './category-main.component.html',
+  styleUrls: ['./category-main.component.css']
+})
+export class CategoryMainComponent {
+
+}
---
+++ src/app/clientApp/pages/category/category.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryMainComponent } from './category-main/category-main.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoryMainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryModule { }
