92041ed52b649f279359f95fbdde513360acd792
wicho <wichocanul07@gmail.com>
2023-10-06 10:23:18 -0600

    SAF: add router Link in auth section and nav section

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['../login/login.component.css']
+  styleUrls: ['../login/login.component.css'],
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  login() {
+    this.router.navigate(['/auth']);
+  }
 }
