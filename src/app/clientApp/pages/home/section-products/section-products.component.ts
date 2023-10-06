2f09844225fad298b9f02baf817e1238ad7ebc31
wicho <wichocanul07@gmail.com>
2023-10-06 00:00:19 -0600

    SAF: create input for products section

--- src/app/clientApp/pages/home/section-products/section-products.component.ts
+++ src/app/clientApp/pages/home/section-products/section-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-section-products',
@@ -6,6 +6,8 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./section-products.component.css']
 })
 export class SectionProductsComponent {
+
+  @Input() title: string = '';
 
   @ViewChild('asContainerCards') containerCards!: ElementRef;
 
