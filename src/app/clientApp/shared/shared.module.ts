cd41c60a2b888799b81bb87a6a7ac3022d8e237f
wicho <wichocanul07@gmail.com>
2023-10-05 20:53:29 -0600

    SAF: Add footer with styles

--- src/app/clientApp/shared/shared.module.ts
+++ src/app/clientApp/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { ComponentsModule } from '../components/components.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     ComponentsModule
   ],
   exports: [
-    NavComponent
+    NavComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
