c41bdb7989f82522ea78b6cafb0c7cbc24211468
wicho <wichocanul07@gmail.com>
2023-10-06 10:03:16 -0600

    SAF: Add target category component

--- src/app/clientApp/components/components.module.ts
+++ src/app/clientApp/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { TargetScrollComponent } from './target-scroll/target-scroll.component';
+import { TargetCategoryComponent } from './target-category/target-category.component';
 
 
 
 @NgModule({
   declarations: [
     SearchComponent,
-    TargetScrollComponent
+    TargetScrollComponent,
+    TargetCategoryComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SearchComponent,
-    TargetScrollComponent
+    TargetScrollComponent,
+    TargetCategoryComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/clientApp/components/target-category/target-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-target-category',
+  templateUrl: './target-category.component.html',
+  styleUrls: ['./target-category.component.css']
+})
+export class TargetCategoryComponent {
+
+}
