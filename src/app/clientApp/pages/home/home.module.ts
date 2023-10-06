095931a8c681869fb63a6c32bc663d81b1bb735f
wicho <wichocanul07@gmail.com>
2023-10-05 22:56:14 -0600

    SAF: Add products in module section

--- src/app/clientApp/pages/home/home.module.ts
+++ src/app/clientApp/pages/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home-main/home.component';
 import { SliderHomeComponent } from './slider-home/slider-home.component';
+import { SectionProductsComponent } from './section-products/section-products.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SliderHomeComponent
+    SliderHomeComponent,
+    SectionProductsComponent
   ],
   imports: [
     CommonModule
